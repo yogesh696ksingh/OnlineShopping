2fca50172d44dcdf1c726172ecba3d60e1c31ad8
yogesh696ksingh <yogesh696ksingh@gmail.com>
2018-12-03 10:23:11 +0530

    add KuT snaps

---
+++ src/main/java/com/os/model/BillingDetails.java
@@ -0,0 +1,48 @@
+package com.os.model;
+
+public class BillingDetails {
+	private int pk_g11_billing;
+	private int gp_id;
+	private int gu_id;
+	private int gb_quantity;
+	private String gp_name;
+	private String gp_price;
+	public int getPk_g11_billing() {
+		return pk_g11_billing;
+	}
+	public void setPk_g11_billing(int pk_g11_billing) {
+		this.pk_g11_billing = pk_g11_billing;
+	}
+	public int getGp_id() {
+		return gp_id;
+	}
+	public void setGp_id(int gp_id) {
+		this.gp_id = gp_id;
+	}
+	public int getGu_id() {
+		return gu_id;
+	}
+	public void setGu_id(int gu_id) {
+		this.gu_id = gu_id;
+	}
+	public int getGb_quantity() {
+		return gb_quantity;
+	}
+	public void setGb_quantity(int gb_quantity) {
+		this.gb_quantity = gb_quantity;
+	}
+	public String getGp_name() {
+		return gp_name;
+	}
+	public void setGp_name(String gp_name) {
+		this.gp_name = gp_name;
+	}
+	public String getGp_price() {
+		return gp_price;
+	}
+	public void setGp_price(String gp_price) {
+		this.gp_price = gp_price;
+	}
+	
+	
+}
